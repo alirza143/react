9002801dcd46dd5f7bd2d44f146f3c5209b177d3
Your Name <you@example.com>
2022-09-20 14:26:41 +0500

    how to use variable in react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  // Create a varible here
+  const title = "welcome to the first react app";
+  const likes = 55;
+  const link = 'https://www.google.com';
   return (
     <div className="App">
       <h1>
-        <div className="content">App Component</div>
+        {/* call the variable that we created earlier */}
+        <div className="content">{ title }</div>
+        <p>likes it { likes } time</p>
+        <p>array {[1,2,3,4,5,6]}</p>
+        <a href={ link }>
+          google link
+        </a>
       </h1>
     </div>
   );
