8a954920fbcf61738bd3ac8d119221e06a3ee023
Your Name <you@example.com>
2022-09-20 16:45:52 +0500

    add multiple component to react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
-  // Create a varible here
-  const title = "welcome to the first react app";
-  const likes = 55;
-  const link = 'https://www.google.com';
   return (
     <div className="App">
-      <h1>
-        {/* call the variable that we created earlier */}
-        <div className="content">{ title }</div>
-        <p>likes it { likes } time</p>
-        <p>array {[1,2,3,4,5,6]}</p>
-        <a href={ link }>
-          google link
-        </a>
-      </h1>
+      <Navbar />
+      <div className="content">
+      <Home />
+        </div>
+      
     </div>
   );
 }
