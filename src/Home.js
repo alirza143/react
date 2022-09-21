68662b2b57a961dd4a830d86230144c406d14b0f
Your Name <you@example.com>
2022-09-21 13:00:30 +0500

    add click event, learn about anonymous function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("clicked");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log(name)
+    }
     return ( 
-        
-            <h1>Home Component</h1>
+            <div className="home">
+                <h1>Home Component</h1>
+                    <button onClick={handleClick}>Click me</button>
+                    <button onClick={() => {
+                        handleClickAgain("you are clicked again")
+                    } }>Click me</button>
+            </div>
        
      );
 }
