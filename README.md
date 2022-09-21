87b577dd9d092057eaa6d27eb99e21554750d6f6
Your Name <you@example.com>
2022-09-21 13:59:59 +0500

    resuing and filtering the data

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-# This Lessson about props Properties
+# This Lessson about Resuing the component and filtering the component
 
-we need to resue some piece of code/component/templete in react app.
-
-def : props is use to pass the data from parent component to child component.
-
-for this we use props.
-
-let see blogs list is use in different area of project.
-we create new component and move that listing templete code to new BlogList component
-
-Now BlogList templete need to be populated but component code is in Home component
-
-first we can paste the code to BlogList component. but by this approch we can not use the same component code.
+we can reuse the same templet and can perform action on it according to the data.
 
 
 
--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,15 @@ import BlogList from "./BlogList";
 const Home = () => {
     
     const [blogs, setBlogs] = useState([
-        { title: 'My new website' , body: 'lorem ipsum ...', author: ' mario' , id: 1 },
+        { title: 'My new website' , body: 'lorem ipsum ...', author: 'mario' , id: 1 },
         { title: 'Welcome party! ', body: ' lorem ipsum ...', author: 'yoshi' , id: 2 },
-        { title: 'Web dev top tips' , body: ' lorem ipsum ...' , author: ' mario' , id: 3 }
+        { title: 'Web dev top tips' , body: ' lorem ipsum ...' , author: 'mario' , id: 3 }
         ]);
 
     return ( 
             <div className="home">
-                <BlogList blogs={blogs}/>
+                <BlogList blogs={blogs} title = "All Blogs"/>
+                <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title = "Marios's Blogs"/>
             </div>
        
      );
