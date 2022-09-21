38bad8614d3889985c7995facdf4d127addf9ef1
Your Name <you@example.com>
2022-09-21 13:34:40 +0500

    we list the blogs/data

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,22 @@ import { useState } from "react";
 
 const Home = () => {
     
-    const [name, setName] = useState('marrio');
-
-    const [age, setAge] = useState(50);
-
-    const handleClick = () => {
-       setName("Waqqar");
-       setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website' , body: 'lorem ipsum ...', author: ' mario' , id: 1 },
+        { title: 'Welcome party! ', body: ' lorem ipsum ...', author: 'yoshi' , id: 2 },
+        { title: 'Web dev top tips' , body: ' lorem ipsum ...' , author: ' mario' , id: 3 }
+        ]);
 
     return ( 
             <div className="home">
-                <h1>Home Component</h1>
-                <p>{ name } is {age} old Janab</p>
-                    <button onClick={handleClick}>Click me</button>
+                {
+                    blogs.map((blog) => (
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>written by {blog.author}</p>
+                        </div>
+                    ))
+                }
             </div>
        
      );
