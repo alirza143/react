99907bcc99f7eebfd8fa87e02341c07f16a571b3
Your Name <you@example.com>
2022-09-21 15:10:08 +0500

    use Hook useEffect, dependecy of useEffect

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# This Lessson about usign props as finction for deleting.
+# This Lessson about useEffect Hook
 
-we can reuse the same templet and can perform action on it according to the data.
+useState rander the dom. when refresh and when state changes mean when data is change.
+
+useEffect to run the code on every rander
+
+# This Lessson about add dependencies to useEffect. 
+
+add dependenciy to useEffect that if that dependenciy change function run otherwise dont 
 
 
 
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,15 +8,30 @@ const Home = () => {
         { title: 'Welcome party! ', body: ' lorem ipsum ...', author: 'yoshi' , id: 2 },
         { title: 'Web dev top tips' , body: ' lorem ipsum ...' , author: 'mario' , id: 3 }
         ]);
+
+    const [name, setName] = useState('mario');
+
     const deleteBlog = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    const changeName = (name) => {
+        setName(name);
+    }
+
+    // const is not using beacuse it does not return anything.
+    // passing empty dependenciy array will only run the function once.
+    // passing value in array will add dependency with name
+    useEffect(() => {
+        console.log("use effect run");
+    }, [name]);
     return ( 
-            <div className="home">
-                <BlogList blogs={blogs} title = "All Blogs" handleDelete = {deleteBlog}/>
-            </div>
-       
+        <div className="home">
+            <BlogList blogs={blogs} title = "All Blogs" handleDelete = {deleteBlog}/>
+            <p>{ name }</p>
+            <button onClick={() => changeName("waqqar")}>Change</button>
+        </div>
      );
 }
  
