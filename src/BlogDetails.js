8a8cce7b7bf56cffd3d69a71af5d4e18eea360b4
Your Name <you@example.com>
2022-10-09 03:47:01 +0500

    deleting the record/data from db

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,20 @@
-import { useParams } from 'react-router-dom';
+import { useParams , useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const {data : blog, isPending, isError : error} = useFetch(`http://localhost:8000/blogs/${id}`)
+    const history =  useHistory();
 
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`,{
+            method : "DELETE"
+        }).then(() => {
+            history.push('/');
+            console.log('delete');
+        })
+    }
     return ( 
         <div className="blog-details">
             { isPending && <div>Loading...</div>}
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             ) }
         </div>
